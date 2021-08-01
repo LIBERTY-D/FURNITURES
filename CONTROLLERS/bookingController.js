7d2238775f9625364abb0bd7c54beea96a892463
LIBERTY-D <danielliberty2425@gmail.com>
2021-08-01 18:23:59 +0200

    Refactored Webhook Controller

--- CONTROLLERS/bookingController.js
+++ CONTROLLERS/bookingController.js
@@ -85,17 +85,37 @@ module.exports.webhook = async (req, res, next) => {
       process.env.endpointSecret
     );
   } catch (err) {
-    res.status(400).send(`Webhook Error: ${err.message}`);
-  }
-  // Handle the event
-  if (event.type === "checkout.session.completed") {
-    await Booking.create({
-      user: event.data.object.client_reference_id,
-      userProducts: JSON.parse(event.data.object.metadata.userProducts),
-    });
+    return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
-  return res.status(200).json({
-    received: true,
-  });
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      await Booking.create({
+        user: event.data.object.client_reference_id,
+        userProducts: JSON.parse(event.data.object.metadata.userProducts),
+      });
+      break;
+    case "payment_intent.succeeded":
+      const paymentIntent = event.data.object;
+      console.log("PaymentIntent was successful!");
+      break;
+    case "payment_method.attached":
+      const paymentMethod = event.data.object;
+      console.log("PaymentMethod was attached to a Customer!");
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a response to acknowledge receipt of the event
+  res.json({ received: true });
+  // Handle the event
+  // if (event.type === "checkout.session.completed") {
+  //   await Booking.create({
+  //     user: event.data.object.client_reference_id,
+  //     userProducts: JSON.parse(event.data.object.metadata.userProducts),
+  //   });
+  // }
 };
