3a255ccb4d44adfb9b6dd172b951555ac525ec36
LIBERTY-D <danielliberty2425@gmail.com>
2021-08-01 13:17:09 +0200

    enabled trust proxy and other testing minor chhanges

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const pug = require("pug");
 const compression = require("compression");
 const router = express.Router();
 const app = express();
-
+app.enable("trust proxy");
 const DB = require("./server");
 const userController = require("./CONTROLLERS/userController");
 // ERROR HANDLERS
@@ -16,18 +16,24 @@ app.set("view engine", "pug");
 app.set("views", `${path.join(__dirname, "VIEWS")}`);
 // VIEW CONROLLERS
 const viewController = require("./CONTROLLERS/viewController");
+const bookingController = require("./CONTROLLERS/bookingController");
 const productRouter = require("./ROUTERS/productsRouter");
 const userRouter = require("./ROUTERS/userRouter");
 const reviewRouter = require("./ROUTERS/reviewRouter");
 const bookingRouter = require("./ROUTERS/bookingRouter");
 const viewsRouter = require("./ROUTERS/viewsRouter");
-//PASS INCOMING MESSAGES
+
+// WEBHOOK STRIPE
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  bookingController.webhook
+);
 app.use(express.json());
 // COOKIE PARSER
 app.use(cookieParser());
 // STATIC FILES
 app.use(express.static(`${path.join(__dirname, "PUBLIC")}`));
-
 app.use("/furnitures", router);
 router.route("/").get(userController.isLoggedIn, viewController.home);
 router.route("/All").get(userController.isLoggedIn, viewController.All);
@@ -77,6 +83,12 @@ app.use(err.errorHandler);
 DB(process.env.DB_CONNECTION.replace("<PASSWORD>", process.env.password));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Listening PORT:${PORT}...`);
+});
+process.on("SIGTERM", () => {
+  console.log("SIGTERM RECEIVED....");
+  server.close(() => {
+    console.log("Server Terminated");
+  });
 });
